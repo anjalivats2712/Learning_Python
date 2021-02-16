1525cc6acbf99c3811725a8d0161ed2d51633af3
anjalivats2712 <anjalivats2712@gmail.com>
2021-02-16 13:15:04 -0800

    updated

--- anjali/practice-day1.py
+++ anjali/practice-day1.py
@@ -9,3 +9,11 @@ print(myDict)
 print(myDict['school'])
 print(myDict['pen'])
 print(myDict['anotherdict']['wreck it'])
+#dictionaries methods
+print(myDict.keys())
+print(myDict.values())
+print(myDict.items())
+updatedone={'cartoon':'animation'}
+myDict.update(updatedone)
+print(myDict)
+print(myDict.get('school'))#it will not throw an error if word is not present in the dictionary
