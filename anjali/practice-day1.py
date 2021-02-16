e1e7a5a5a74bf8080a457820dc065ed529daf1c9
anjalivats2712 <anjalivats2712@gmail.com>
2021-02-16 13:04:01 -0800

    dictionary

---
+++ anjali/practice-day1.py
@@ -0,0 +1,11 @@
+#dictionaries
+myDict = {'book':'we read',
+'school':'we go',
+'pen':'we write',
+'anotherdict':{'wreck it':'destroy it',
+'venelope':'chracter',},
+'prayer':'school'}
+print(myDict)
+print(myDict['school'])
+print(myDict['pen'])
+print(myDict['anotherdict']['wreck it'])
